fbce56d5b7dba85a5a7dce021f3d9d68af214424
Piotr Tekieli <p.s.tekieli@student.tudelft.nl>
2016-12-16 20:48:06 +0100

    Cleaning up exp branch
    Cleaning up

--- infovis/js/app.js
+++ infovis/js/app.js
@@ -1,38 +1,52 @@
 $(document).foundation();
 
-var AvailableSeries = [], DataSeries = [];
-AvailableSeries.length = 0;
-DataSeries.length = 0;
+var NURI = "data/wdi/wdi_numbers.json", PURI = "data/wdi/wdi_percent.json", ActiveSeries, ActiveYear
+AvailableNumericSeries = [], AvailablePercentageSeries = [], 
+Categories = [], Years = [], DataSeries = [];
+AvailableNumericSeries.length = 0, AvailablePercentageSeries.length = 0, 
+Categories.length = 0, Years.length = 0, DataSeries.length = 0;
 
-d3.csv("data/wdi/wdi.csv", function(loadedRows) {
-   var WDI_CSV = loadedRows;  
-   PopulateForms(WDI_CSV);
-});
+function PopulateArray(database, uri) {
+    if (uri === NURI) { AvailableNumericSeries = database; }
+    else { AvailablePercentageSeries = database; };
+};
 
-function PopulateForms(WDI_CSV) {
-    /* Populate categories of MAP section, set default value (5th series code)*/
-    for (row of WDI_CSV) {
-        if (($.inArray(row["Series Name"], AvailableSeries)) === (-1)) {
-                AvailableSeries.push(row["Series Name"]);
-                $("#selectable").append("<option value=\"" + row["Series Code"] + "\">" +
-                    row["Series Name"] + "</option>");
-            };
+function AddToMapCategoryForm(row) {
+    if (($.inArray(row["series_name"], Categories)) === (-1)) {
+        Categories.push(row["series_name"]);
+        $("#selectable").append("<option value=\""+row["series_code"]+"\">" + row["series_name"] + "</option>");
     };
-    $("#selectable option[value=\"" + WDI_CSV[4]["Series Code"] + "\"]").prop("selected", "selected");
-    AvailableSeriesDefault = AvailableSeries[4];       
-    row = null;
-    var temp_counter = 0;
-    for (row of WDI_CSV) {
-        if (row["Series Name"] === AvailableSeriesDefault) {            
-            DataSeries.push(row["2014 [YR2014]"]);
-        }
-    }
+};
+
+function PopulateForms(series) {
+    ActiveSeries = series;    
+    /* Populate categories of MAP section, set default value (5th series code)*/
+    for (row of series) {
+        AddToMapCategoryForm(row);       
+    }; 
+
     /* Populate years of MAP section, set default value (2014) */    
     for (i = 2000; i < 2015; i++) {
-        $("#selectable2").append("<option value=\"" + "[YR" + i + "]" + "\">" + i + "</option>");  
+        $("#selectable2").append("<option value=\"" + "yr" + i + "\">" + i + "</option>");  
     };
-    $("#selectable2 option[value=\"[YR" + 2014 + "]\"").prop("selected", "selected");
+    $("#selectable2 option[value=\"yr" + 2014 + "\"").prop("selected", "selected");
+
+    AvailableSeriesDefault = series[0]["series_code"]; 
+    ActiveYear = AvailableSeriesYDefault = "yr2014";   
+
+    PushDataToMap(series, AvailableSeriesDefault, AvailableSeriesYDefault)
+    
 };
+
+function PushDataToMap(series, category, year) {
+    DataSeries.length = 0;
+    for (row of series) {
+        if (row["series_code"] === category) {            
+            DataSeries.push(row[year]);
+        };
+    }
+    LoadMap();
+}
 
 /* Initialize MAP Module */
 var map = L.map('map').setView([52.520008, 13.404954], 4);
@@ -41,3 +55,7 @@ var legend = L.control({position: 'bottomright'});
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJnb25lIiwiYSI6ImNpdzJidTR6bTAwMXAyeW15N3hsa2c0NnIifQ.KPL00OFjX6lRS8W5yN0eYA', {
     maxZoom: 5    
 }).addTo(map);
+
+$.getJSON(NURI, function(data) { PopulateArray(data, NURI) });
+$.getJSON(PURI, function(data) { PopulateArray(data, PURI); PopulateForms(AvailablePercentageSeries); });
+
