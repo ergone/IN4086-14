ff101dd29dd1f5dfca6bc29ef542128450949762
ulinnuha <md.ulinnuha@gmail.com>
2016-12-20 01:50:48 +0100

    add line chart to overall graph

--- infovis/js/interaction.js
+++ infovis/js/interaction.js
@@ -1,8 +1,19 @@
 $('#selectable').on('change', function() {   
     PushDataToMap(AvailablePercentageSeries, $(this).val(), $("#selectable2").val());
     drawChart();
+
+    var currSeriesCode = $(this).val();
+    $("#curr-series-code").val(currSeriesCode + ".V");
+    drawLineChart();
 });
 
 $('#selectable2').on('change', function() {   
     PushDataToMap(AvailablePercentageSeries, $("#selectable").val(), $(this).val());
+
+    var currYear = $(this).val();
+    $("#curr-year").val(currYear);
 });
+
+$('#curr-series-code').on('change', function(){
+    drawLineChart();
+})
